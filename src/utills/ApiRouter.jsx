d387b07aa1f3daef37be27022539460375d31b89
SUBROTOKOIRY <koirysubroto1103@gmail.com>
2024-08-30 02:07:12 +0530

    change in the host address to localhost

--- src/utills/ApiRouter.jsx
+++ src/utills/ApiRouter.jsx
@@ -1,4 +1,4 @@
-const host = 'http://18.207.225.92:5000'
+const host = 'http://localhost:5000'
 const registerRoute = `${host}/api/auth/Register`
 const loginRoute = `${host}/api/auth/login`
 const setProfilePictureRoute = `${host}/api/auth/setProfilePicture`
