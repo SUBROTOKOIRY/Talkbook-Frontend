9b4ab6c28795621ad5c10191aece276fe0d9f10d
SUBROTOKOIRY <koirysubroto1103@gmail.com>
2024-09-01 20:48:58 +0530

    connecting the frontend to render backend"

--- src/utills/ApiRouter.jsx
+++ src/utills/ApiRouter.jsx
@@ -1,4 +1,5 @@
-const host = 'http://18.207.225.92:5000'
+// const host = 'http://18.207.225.92:5000'
+const host="https://subroto-8wob.onrender.com"
 const registerRoute = `${host}/api/auth/Register`
 const loginRoute = `${host}/api/auth/login`
 const setProfilePictureRoute = `${host}/api/auth/setProfilePicture`
